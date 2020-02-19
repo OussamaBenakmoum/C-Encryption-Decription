eb66b30f613da4c8261b98ab4a840f862373aacd
OussamaBenakmoum <io.benakmoum@gmail.com>
2020-02-19 12:38:14 +0100

    main project.

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+typedef enum{False, True} Boolean;
+unsigned char choice ;
+int comp_select;
+
+
+int main()
+{
+        initialisation();
+        open_files();
+        choix_crypt();
+
+    return 0;
+
+}
